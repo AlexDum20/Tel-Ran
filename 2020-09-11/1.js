b8261807bf8b5709cba55eb5831b6cc8c7acd4fc
AlexDum20 <dumchevas@yahoo.com>
2020-09-11 18:35:14 +0300

    delete answers

--- 2020-09-11/1.js
+++ 2020-09-11/1.js
@@ -7,10 +7,7 @@ let id2 = Symbol('id2');
 console.log(id1,id2, id1==id2);
 
 const answers = [];
-// answers[0] = prompt('Как ваше имя?','');
-// answers[1] = prompt('Как ваша фамилия?','');
-// answers[2] = prompt('Сколько вам лет?','');
-// document.write(answers);
+
 console.log(typeof(answers));
 console.log(typeof(null));
 
