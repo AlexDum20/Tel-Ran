4e6c5864a8a6d2b7a32a38b362478576a86e99d3
AlexDum20 <dumchevas@yahoo.com>
2020-09-18 21:34:05 +0300

    homework task done - ver.2

--- 2020-09-18/HomeWork/trafficLights.js
+++ 2020-09-18/HomeWork/trafficLights.js
@@ -1,27 +1,45 @@
-let but = document.querySelectorAll("[butName]"),
-    sq = document.querySelectorAll("[divName]"),
+// let but = document.querySelectorAll("[butName]");
+let    sq = document.querySelectorAll("[divName]"),
     clean = document.querySelector("[but]");
+
+document.querySelectorAll("[butName]").forEach((btn,i) =>{
+    btn.addEventListener('click', () =>{
+        if(btn.matches("[butName='red']")){
+            sq[0].classList.add("red");
+            sq[1].classList.remove("yellow");
+            sq[2].classList.remove("green");
+        }
+        if (btn.matches("[butName='yellow']")){
+            sq[1].classList.add("yellow");
+            sq[0].classList.remove("red");
+            sq[2].classList.remove("green");
+        }
+        if (btn.matches("[butName='green']")){
+            sq[2].classList.add("green");
+            sq[1].classList.remove("yellow");
+            sq[0].classList.remove("red");
+        }
+    })
+})
+
 
 clean.addEventListener('click',function (){
     sq[0].classList.remove("red");
     sq[1].classList.remove("yellow");
     sq[2].classList.remove("green");
 })
-but[0].addEventListener('click', function (){
-    // console.log(butRed);
-    sq[0].classList.add("red");
-    sq[1].classList.remove("yellow");
-    sq[2].classList.remove("green");
-});
-but[1].addEventListener('click', function (){
-    // console.log(butRed);
-    sq[1].classList.add("yellow");
-    sq[0].classList.remove("red");
-    sq[2].classList.remove("green");
-});
-but[2].addEventListener('click', function (){
-    // console.log(butRed);
-    sq[2].classList.add("green");
-    sq[1].classList.remove("yellow");
-    sq[0].classList.remove("red");
-});
+// but[0].addEventListener('click', function (){
+//     sq[0].classList.add("red");
+//     sq[1].classList.remove("yellow");
+//     sq[2].classList.remove("green");
+// });
+// but[1].addEventListener('click', function (){
+//     sq[1].classList.add("yellow");
+//     sq[0].classList.remove("red");
+//     sq[2].classList.remove("green");
+// });
+// but[2].addEventListener('click', function (){
+//     sq[2].classList.add("green");
+//     sq[1].classList.remove("yellow");
+//     sq[0].classList.remove("red");
+// });
