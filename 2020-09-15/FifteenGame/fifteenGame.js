df21f6f0bace846063d37f7127248481fd0ae441
AlexDum20 <dumchevas@yahoo.com>
2020-09-19 21:30:12 +0300

    homework task done - added randomize

--- 2020-09-15/FifteenGame/fifteenGame.js
+++ 2020-09-15/FifteenGame/fifteenGame.js
@@ -3,14 +3,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const cell = document.querySelectorAll('.section'),
-        wrapper = document.querySelector('.main');
+        wrapper = document.querySelector('.main'),
+        newGame = document.querySelector("[but]"),
+        victory = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', ''];
 
     wrapper.addEventListener('click', (event) => {
         let a = event.target,
             i = event.target.classList.item(1) - 10,
             j = Number(event.target.classList.item(2));
-        // console.dir(event.target);
-        // console.log(i, j, i - 1, j - 1);
 
         let pointsMotion = [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]];
         for (let k = 0; k < 4; k++) {
@@ -21,11 +21,42 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (!target.innerText) {
                     target.innerText = a.innerText;
                     a.innerText = "";
+                    console.log(Math.round(15 * Math.random()));
+                    if (verifyVictory()) alert("Victory!");
                 }
             }
         }
 
     });
 
+    newGame.addEventListener('click', function () {
+        setRandom();
+    })
 
+    function verifyVictory() {
+        for (let i = 0; i <= 15; i++) {
+            if (cell[i].innerText != victory[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function setRandom() {
+        let newArray = victory.slice();
+        for (let i = 0; i <= 14; i++) {
+            function install(i) {
+                let j = Math.round(15 * Math.random());
+                if (newArray[j]) {
+                    cell[i].innerText = newArray[j];
+                    newArray[j] = '';
+                } else {
+                    install(i);
+                }
+            }
+
+            cell[i].innerText = '';
+            install(i);
+        }
+    }
 });
