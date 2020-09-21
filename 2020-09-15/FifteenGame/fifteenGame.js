9e1606ac7855298471757fbcb64f9a2ddd6b6b25
AlexDum20 <dumchevas@yahoo.com>
2020-09-21 13:43:42 +0300

    reformat code

--- 2020-09-15/FifteenGame/fifteenGame.js
+++ 2020-09-15/FifteenGame/fifteenGame.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const cell = document.querySelectorAll('.section'),
         wrapper = document.querySelector('.main'),
         newGame = document.querySelector("[but]"),
+        // motion = ['down','up','right','left'],
         victory = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', ''];
+
 
     wrapper.addEventListener('click', (event) => {
         let a = event.target,
@@ -14,20 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let pointsMotion = [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]];
         for (let k = 0; k < 4; k++) {
-            // console.log("all ", k, pointsMotion[k])
             if (pointsMotion[k][0] > 0 && pointsMotion[k][0] <= 4 && pointsMotion[k][1] > 0 && pointsMotion[k][1] <= 4) {
                 let cl = ".row" + pointsMotion[k][0] + pointsMotion[k][1];
                 let target = document.querySelector(cl);
                 if (!target.innerText) {
                     target.innerText = a.innerText;
                     a.innerText = "";
-                    a.style.backgroundColor = "lightblue";
-                    a.style.border = "0";
-                    target.style.backgroundColor = "white";
-                    target.style.border = "1px solid blue";
-                    target.style.borderRadius = "10px";
-                    console.dir(target.classList);
-                    console.dir(a.classList);
+                    a.classList.toggle("empty");
+                    target.classList.toggle("empty");
                     if (verifyVictory()) alert("Victory!");
                 }
             }
@@ -55,6 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 let j = Math.round(15 * Math.random());
                 if (newArray[j]) {
                     cell[i].innerText = newArray[j];
+                    cell[i].classList.remove("empty")
                     newArray[j] = '';
                 } else {
                     installFor(i);
@@ -64,5 +61,46 @@ document.addEventListener('DOMContentLoaded', () => {
             cell[i].innerText = '';
             installFor(i);
         }
+        cell[15].innerText = '';
+        cell[15].classList.add("empty");
     }
+
+    // function myAnimation(elem,start){
+    //     let pos,  move, plusMinus, stop;
+    //     pos = 0;
+    //     if (start==0 || start==1) {
+    //         move = 'y';
+    //     } else {
+    //         move = 'x';
+    //     }
+    //     if (start==0 || start==2) {
+    //         plusMinus = '-';
+    //         stop = pos - 98;
+    //     } else {
+    //         plusMinus = '+';
+    //         stop = pos + 98;
+    //     }
+    //
+    //     const id = setInterval(frame, 10);
+    //     function frame(){
+    //         if (pos==stop){
+    //             clearInterval(id);
+    //         } else {
+    //             if (plusMinus == '+'){
+    //                 pos++;
+    //             } else {
+    //                 pos--;
+    //             }
+    //             if (move == 'y') {
+    //                 elem.style.top = pos + "px";
+    //                 console.log(pos);
+    //             } else {
+    //                 elem.style.left = pos + "px";
+    //                 console.log('Hello');
+    //             }
+    //         }
+    //     }
+    // }
+
 });
+
