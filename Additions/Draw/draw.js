0c63c6013bceee4d0d27f16d9e33d52f58c39fa0
AlexDum20 <dumchevas@yahoo.com>
2020-10-31 13:16:28 +0200

    home task

---
+++ Additions/Draw/draw.js
@@ -0,0 +1,57 @@
+//creating a field for drawing
+
+let wrap = document.createElement("div");
+wrap.classList.add("wrap");
+for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+        let sq = document.createElement("div");
+        sq.classList.add("sq", "small");
+        sq.style.left = j * 5 + "px";
+        sq.style.top = i * 5 + "px";
+        wrap.appendChild(sq);
+    }
+}
+document.body.appendChild(wrap);
+
+// creating a block for selecting a color
+
+let title = document.createElement('span');
+title.innerText = "Choice of color";
+let draw = document.createElement("input");
+draw.classList.add("input");
+draw.type = "color";
+draw.value = "#ff0000";
+draw.placeholder = "choice of color";
+let wrapDiv = document.createElement("div");
+wrapDiv.classList.add("wrapdiv");
+wrapDiv.appendChild(title);
+wrapDiv.appendChild(draw);
+document.body.appendChild(wrapDiv);
+
+//draw event
+
+wrap.addEventListener("mousedown", function (e) {
+
+    wrap.addEventListener("mousemove", mmove);
+    wrap.addEventListener("mouseup", mup);
+    wrap.addEventListener("mouseleave",mup);
+
+    function mmove(emove) {
+        let target = emove.target;
+        (target.localName === "div" && target.className === 'sq small') ?
+            target.style.backgroundColor = draw.value:'';
+    }
+
+    function mup() {
+        wrap.removeEventListener("mousemove", mmove);
+        wrap.removeEventListener("mouseup", mup);
+        wrap.addEventListener("mouseleave",mup);
+    }
+
+});
+
+
+
+
+
+
